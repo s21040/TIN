bc6d1879d8a1cb765f729bc57f7b3a036d4fe307
s21040 <92230173+s21040@users.noreply.github.com>
2021-11-03 11:37:02 +0100

    Create 4f.js

---
+++ Lab4/4f.js
@@ -0,0 +1,15 @@
+function checkPrime(n) {
+    if (n === 1) {
+        return false;
+    } else if(n === 2) {
+        return true;
+    }else {
+        for(let i = 2; i < n; i++) {
+            if(n % i === 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+console.log(checkPrime(2));
